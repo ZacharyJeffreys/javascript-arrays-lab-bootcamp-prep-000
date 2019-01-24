17f2edbf701c96974210912a065f1b80245d4639
Zachary Jeffreys <zachary.jeffreys@wsu.edu>
2019-01-24 23:43:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,4 +9,8 @@ function destructivelyPrependKitten(name){
   kittens.unshift(name);
   return kittens;
 }
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
 function 
