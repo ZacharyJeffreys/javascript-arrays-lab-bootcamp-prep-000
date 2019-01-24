fae6c4350df4f11c1f61646af381e8591af9104e
Zachary Jeffreys <zachary.jeffreys@wsu.edu>
2019-01-24 23:42:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var kittens = ['Milo', 'Otis', 'Garfield'];
 // Add your functions and code here
 function destructivelyAppendKitten(name){
   kittens.push(name);
-  return kittens
+  return kittens;
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
 }
 function 
